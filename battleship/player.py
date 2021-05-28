456c4fdd8987302b4fb8de67f7e2e95dc8d7f55c
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-05-28 23:54:36 +0200

    Integrate comunication with server with Player class

--- battleship/player.py
+++ battleship/player.py
@@ -1,4 +1,6 @@
 import os
+import socket
+import sys
 from .board import BattleShipBoard
 from .ship import Ship
 
@@ -14,11 +16,47 @@ def text_to_cords(text_cords):
 class BattleShipPlayer():
     """ BattleShip player class """
 
-    def __init__(self):
+    def __init__(self, hostname="localhost", port=6060):
         self.board = BattleShipBoard()
         self.board_to_shots = BattleShipBoard()
         self.ships = []
+        self.hostname = hostname
+        self.port = port
+        self.current = False
         self.new_ships_set()  # Deploy ships on board
+        self.client = self.connect_to_server()  # Set up connection
+
+    def connect_to_server(self):
+        """ Connect to server and run main player loop """
+
+        try:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect((self.hostname, self.port))
+            return client
+        except Exception as e:
+            print("Can't connect to server: ", e)
+            sys.exit()
+
+    def player_loop(self):
+        """ Main player loop """
+        
+        while True:
+            if not self.current:
+                msg = self.client.recv(1024)
+                if msg.decode("utf-8") == "current":
+                    self.current = True
+                    continue
+            
+                print("Enemy shot: ", msg.decode('utf8')[1:])  # Catch shot
+                self.client.send("CMISSED".encode("utf-8"))  # Check shot
+            else:
+                shot_msg = input("Your shot: ")
+                self.client.send(("S" + shot_msg).encode("utf-8"))  # Send shot
+                
+                # Catch check
+                check_msg = self.client.recv(1024)
+                print("Check from enemy: ", check_msg.decode('utf-8')[1:])
+                self.current = False  # Turn off player
 
     def new_ships_set(self):
         """ Create all ships and add to board """
